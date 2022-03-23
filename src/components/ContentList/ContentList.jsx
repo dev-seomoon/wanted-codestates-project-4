e093135e7b6aa30d0b5c56d9654c2b9c37073caa
dev-seomoon <skm1104@daum.net>
2022-03-23 21:18:31 +0900

    Feat: ContentList UI 구현

--- src/components/ContentList/ContentList.jsx
+++ src/components/ContentList/ContentList.jsx
@@ -1,5 +1,20 @@
+import ContentItem from '../ContentItem/ContentItem';
+import './ContentList.sass';
+
 function ContentList() {
-  return <div>ContentList</div>;
+  return (
+    <div className="content_box content_list">
+      <h2>
+        알쓸B잡<span>News</span>
+      </h2>
+      <ContentItem />
+      <ContentItem />
+      <ContentItem />
+      <button type="button" className="more_button">
+        더보기
+      </button>
+    </div>
+  );
 }
 
 export default ContentList;
