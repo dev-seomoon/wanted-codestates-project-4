b1904af3b85155984b275b98446aa7052d8c1724
dev-seomoon <skm1104@daum.net>
2022-03-23 21:18:41 +0900

    Feat: ContentList UI 연결

--- src/tabs/Insight/Insight.jsx
+++ src/tabs/Insight/Insight.jsx
@@ -1,13 +1,14 @@
 import PropTypes from 'prop-types';
+import ContentList from '../../components/ContentList/ContentList';
 import NewContent from '../../components/NewContent/NewContent';
 import SubscriptionBanner from '../../components/SubscriptionBanner/SubscriptionBanner';
 
 function Insight({ tabIndex }) {
   return (
     <div className={`content tab_${tabIndex}`}>
-      Insight
       <NewContent />
       <SubscriptionBanner />
+      <ContentList />
     </div>
   );
 }
--- src/tabs/Medium/Medium.jsx
+++ src/tabs/Medium/Medium.jsx
@@ -1,13 +1,14 @@
 import PropTypes from 'prop-types';
+import ContentList from '../../components/ContentList/ContentList';
 import NewContent from '../../components/NewContent/NewContent';
 import SubscriptionBanner from '../../components/SubscriptionBanner/SubscriptionBanner';
 
 function Medium({ tabIndex }) {
   return (
     <div className={`content tab_${tabIndex}`}>
-      Medium
       <NewContent />
       <SubscriptionBanner />
+      <ContentList />
     </div>
   );
 }
