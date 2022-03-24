c2f8cc364545fcf0f7090bb990f7a98cdda9b01c
dev-seomoon <skm1104@daum.net>
2022-03-24 22:23:47 +0900

    Chore: import slick-slider css files

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,9 @@
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import App from './App';
 import store from './redux/store';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import App from './App';
 
 ReactDOM.render(
   <Provider store={store}>
