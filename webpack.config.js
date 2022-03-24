d1531234be4ba1fddd62b476158128a976f80d87
dev-seomoon <skm1104@daum.net>
2022-03-24 17:50:16 +0900

    Fix: exports is not defined 에러 해결

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,3 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
-
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
@@ -36,7 +34,18 @@ const config = {
         test: /\.(js|jsx)$/i,
         loader: 'babel-loader',
         options: {
-          presets: ['@babel/preset-react', '@babel/preset-env'],
+          presets: [
+            '@babel/preset-react',
+            [
+              '@babel/preset-env',
+              {
+                targets: {
+                  esmodules: true,
+                },
+              },
+            ],
+          ],
+          plugins: ['@babel/transform-runtime'],
         },
       },
       {
@@ -51,9 +60,6 @@ const config = {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
         type: 'asset',
       },
-
-      // Add your rules for custom modules here
-      // Learn more about loaders from https://webpack.js.org/loaders/
     ],
   },
 };
