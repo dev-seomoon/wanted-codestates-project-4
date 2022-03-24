6ccf16abfeb28c304e4e1296f8f2392d02bfe9ea
dev-seomoon <skm1104@daum.net>
2022-03-25 00:12:56 +0900

    Feat: ContentDetail header UI 구현

--- src/components/ContentDetail/ContentDetail.jsx
+++ src/components/ContentDetail/ContentDetail.jsx
@@ -1,5 +1,15 @@
+import { Link } from 'react-router-dom';
+import './ContentDetail.sass';
+
 function ContentDetail() {
-  return <div>ContentDetail</div>;
+  return (
+    <div>
+      <header>
+        <h2>블록체인 NOW</h2>
+        <Link to="/">{`<`}</Link>
+      </header>
+    </div>
+  );
 }
 
 export default ContentDetail;
