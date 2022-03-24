03fab2bd30e658470c308c04a4229efb50c4443e
dev-seomoon <skm1104@daum.net>
2022-03-24 21:21:22 +0900

    Feat: ContentList 데이터 렌더링

--- src/components/ContentList/ContentList.jsx
+++ src/components/ContentList/ContentList.jsx
@@ -1,20 +1,37 @@
+import PropTypes, { number, string } from 'prop-types';
 import ContentItem from '../ContentItem/ContentItem';
 import './ContentList.sass';
 
-function ContentList() {
+function ContentList({ contents }) {
+  if (!contents) return '';
   return (
     <div className="content_box content_list">
       <h2>
         알쓸B잡<span>News</span>
       </h2>
-      <ContentItem />
-      <ContentItem />
-      <ContentItem />
+      {contents.map((c) => (
+        <ContentItem key={c.id} content={c} />
+      ))}
       <button type="button" className="more_button">
         더보기
       </button>
     </div>
   );
 }
+
+ContentList.propTypes = {
+  contents: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: number,
+      title: string,
+      image: string,
+      link: string,
+    })
+  ),
+};
+
+ContentList.defaultProps = {
+  contents: null,
+};
 
 export default ContentList;
