d4df5a468bc74d480e65bd17e7fe0a4edf40b2f3
dev-seomoon <skm1104@daum.net>
2022-03-24 21:21:16 +0900

    Feat: ContentItem 데이터 렌더링

--- src/components/ContentItem/ContentItem.jsx
+++ src/components/ContentItem/ContentItem.jsx
@@ -1,17 +1,22 @@
+import PropTypes, { number, string } from 'prop-types';
 import './ContentItem.sass';
 import HeartIcon from '../../assets/icon_heart.svg';
 import ShareIcon from '../../assets/icon_share.svg';
 
-function ContentItem() {
+function ContentItem({ content }) {
+  if (!content) return '';
+  const { image, link, like_cnt: likeCnt, title, upload_date: uploadDate } = content;
   return (
     <div className="content_item">
-      <img src="https://miro.medium.com/max/1400/1*fq92AQHOLSMqs-w7Pdkm5Q.jpeg" alt="content" />
+      <a href={link}>
+        <img src={image} alt={title} />
+      </a>
       <div className="info">
-        <span>2022-03-15</span>
+        <span>{uploadDate}</span>
         <div className="buttons">
           <button type="button">
             <img src={HeartIcon} alt="like icon" className="icon heart" />
-            <span>1</span>
+            <span>{likeCnt}</span>
           </button>
           <button type="button">
             <img src={ShareIcon} alt="like icon" className="icon" />
@@ -22,5 +27,16 @@ function ContentItem() {
     </div>
   );
 }
+
+ContentItem.propTypes = {
+  content: PropTypes.shape({
+    id: number,
+    title: string,
+    image: string,
+    link: string,
+    upload_date: string,
+    like_cnt: number,
+  }).isRequired,
+};
 
 export default ContentItem;
